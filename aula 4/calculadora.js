7f0bb6174ef99155fc8e1b9648ca247c4b615d6d
Lucas Brito <lucasbsilva29@gmail.com>
2018-09-01 15:59:38 -0300

    arrumando alguns detalhes, logica escrita e tudo separado em função

--- aula 4/calculadora.js
+++ aula 4/calculadora.js
@@ -1,44 +1,96 @@
-function classeHost(ip) {
+function separarIP(ip) {
     ip = ip.split('.');
     
-    val1 = parseInt(ip[0]);
-    
+    oc1 = parseInt(ip[0]);
+    oc2 = parseInt(ip[1]);
+    oc3 = parseInt(ip[2]);
+    oc4 = parseInt(ip[3]);
+
+    return([oc1, oc2, oc3, oc4]);
+};
+
+
+function classeHost(val1) {
     classe = "";
     if (128 > val1 &&  val1 >= 0) {
-        classe = "classe A";
+        classe = "A";
     };
     if (192 > val1 && val1 >= 128) {
-        classe = "classe B";
+        classe = "B";
     };
     if (224 > val1 && val1 >= 192){
-        classe = "classe C";
+        classe = "C";
     };
     if (240 > val1 && val1 >= 224){
-        classe = "classe D";
+        classe = "D";
     };
     if (256 > val1 && val1 >= 240){
-        classe = "classe E";
+        classe = "E";
     };
     return (classe)
-}
+};
 
 
-function transform(ip) {
+function padraoMask(classeHost) {
+    padrao = 0;
+
+    if (classeHost.indexOf("A") == 0) {
+        padrao = 8;
+    };
+    if (classeHost.indexOf("B") == 0) {
+        padrao = 16;
+    };
+    if (classeHost.indexOf("C") == 0){
+        padrao = 24;
+    };
+    if (classeHost.indexOf("D") == 0){
+        /* tirar duvida de qual é o padrão DA CLASSE D e E */
+        padrao = 28;
+    };
+    if (classeHost.indexOf("E") == 0){
+        padrao = 30;
+    };
+
+    return(padrao);
+};
+
+
+function subClasseBin(classeHost) {
+    subclasse = [];
+
+    if (classeHost.indexOf("A") == 0) {
+        subclasse = ["11111111", "00000000", "00000000", "00000000"];
+    };
+    if (classeHost.indexOf("B") == 0) {
+        subclasse = ["11111111", "11111111", "00000000", "00000000"];
+    };
+    if (classeHost.indexOf("C") == 0){
+        subclasse = ["11111111", "11111111", "11111111", "00000000"];
+    };
+    if (classeHost.indexOf("D") == 0){
+        /* tirar duvida de qual é o padrão DA CLASSE D e E */
+        subclasse = ["11111111", "11111111", "11111111", "00000000"];
+    };
+    if (classeHost.indexOf("E") == 0){
+        subclasse = ["11111111", "11111111", "11111111", "00000000"];
+    };
+
+    return(subclasse);
+};
+
+
+function transformarBinIP(ip) {
     ip = ip.split('.');
 
-    ipInt = [];
     ipBinario = [];
 
     ip.forEach(conjunto => {
-        conjunto = parseInt(conjunto);
-        conBinario = parseInt(conjunto.toString(2));
-        ipInt.push(conjunto);
+        conBinario = parseInt(conjunto).toString(2);
         ipBinario.push(conBinario);
     });
-    // console.log(ipInt);
-    // console.log(ipBinario);
+
     return (ipBinario);
-}
+};
 
 
 function quantHost(padraoMask) {
@@ -48,7 +100,8 @@ function quantHost(padraoMask) {
     quant -= 2;
 
     return (quant);
-}
+};
+
 
 function quantSubRedes(padraoMask, padraoDesejado) {
     pm = parseInt(padraoMask);
@@ -56,27 +109,63 @@ function quantSubRedes(padraoMask, padraoDesejado) {
     quant = Math.pow(2,(pm-pd));
 
     return (quant);
-}
+};
+
+
+function mesmaRede(ip1,ip2) {
+    ip1Bin = transformarBinIP(ip1);
+    ip2Bin = transformarBinIP(ip2);
+
+    classeIP1 = classeHost(IP1[0]);
+    classeIP2 = classeHost(IP2[0]);
+    
+    subclasse1 = subClasseBin(classeIP1);
+    subclasse2 = subClasseBin(classeIP2);
+    resultado1 = [];
+    resultado2 = [];
+
+    for (let cont = 0; cont <= ip1Bin.length; cont++) {
+        let blocoIP = ip1Bin[cont];
+        let blocoSub = subclasse[cont]
+        console.log(blocoIP)
+        console.log(blocoSub)
+        // adicionar Zeros a frente de onde só tem 1 caracter
+        // comparar com forEach e adcicionar a lista resultado
+        // repitir o processo para o segundo valor
+        // coparar os dois resultados (se iguais, então estão na mesma rede, se não não)
+    }
+};
 
 
 function main(ip) {
-    padraoMask = 24;
     padraoDesejado = 18;
+    segundoIP = '192.168.0.9';
 
-    classe = classeHost(ip);
+    parteSeparada = separarIP(ip);
+    // console.log(parteSeparada[0]);
+
+    classe = classeHost(parteSeparada[0]);
     // console.log(classe);
 
-    binario = transform(ip);
+    mascaraPadrao = padraoMask(classe);
+    // console.log(mascaraPadrao);
+
+    binario = transformarBinIP(ip);
     // console.log(binario);
 
-    quantiHost = quantHost(padraoMask)
-    // console.log(quantiHost)
+    quantiHost = quantHost(mascaraPadrao);
+    // console.log(quantiHost);
 
-    quantiSubRedes = quantSubRedes(padraoMask, padraoDesejado)
-    console.log(quantiSubRedes)
+    quantiSubRedes = quantSubRedes(mascaraPadrao, padraoDesejado);
+    // console.log(quantiSubRedes);
+
+    subclasse = subClasseBin(classe)
+    // console.log(subclasse);
+
+    redeIgual = mesmaRede(ip, segundoIP);
+    console.log(redeIgual);
 
 }
 
 
 main('192.168.0.1');
-
